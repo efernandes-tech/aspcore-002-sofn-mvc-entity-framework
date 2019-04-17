5ffd29f9a106208a5ad0307acc977a01e643a7bf
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-04-16 22:08:08 -0300

    Models

---
+++ WebCore/Models/Blog/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCore.Models.Blog
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Tilulo { get; set; }
+        public string Resumo { get; set; }
+        public string Url { get; set; }
+    }
+}
