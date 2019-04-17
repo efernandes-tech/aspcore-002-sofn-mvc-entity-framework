7a4f334285b96a5570e0ad01d6e9d5c6f0df6e59
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-04-16 22:18:14 -0300

    Db Context

--- WebCore/Data/ApplicationDbContext.cs
+++ WebCore/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebCore.Models.ManageBlog;
 
 namespace WebCore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Post> Post { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
